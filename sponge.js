cf560d748af3da6cf693962117a6b4465930a653
Rhys Cable <23556567+rhyscable@users.noreply.github.com>
2018-10-19 15:14:15 +0800

    Create sponge.js

---
+++ sponge.js
@@ -0,0 +1,13 @@
+// Sponge.js
+
+exports.sponge = function (str) {
+    if (typeof str !== 'string' || !(str instanceof String)) {
+      return null;
+    }
+
+    returnStr = "";
+    for (let i = 0; i < str.length; i++) {
+      returnStr += (Math.random() >= 0.5) ? ((str[i].toLowerCase() == str[i]) ? str[i].toUpperCase() : str[i].toLowerCase()) : str[i];
+    }
+    return returnStr;
+};
