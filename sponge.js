213dea1e115c8b1e71cdad02c94c2749178d18ab
Mitchell Panicciari <mitchell.panicciari@gmail.com>
2018-10-19 15:26:33 +0800

    Added Comments/Return Statement

--- sponge.js
+++ sponge.js
@@ -1,8 +1,12 @@
 // Sponge.js
 
+//Run this in the browsers console to test it out.
+//sponge(""your string here")
+
 exports.sponge = function (str) {
+    
     if (typeof str !== 'string'){
-      return null;
+      return "What you entered was not a string please pass a string";
     }
 
     returnStr = "";
