0e1470a82db932e480d90ef119a80f9d3ecd2aa6
Rhys Cable <23556567+rhyscable@users.noreply.github.com>
2018-10-19 15:40:26 +0800

    Comments.
    Added comments and a cheeky ASCII art. :+1:

--- sponge.js
+++ sponge.js
@@ -1,18 +1,55 @@
 // Sponge.js
 
-//Run this in the browsers console to test it out.
-//sponge(""your string here")
+/***************************************************
+*                                                  *
+* Run this in the browsers console to test it out. *
+*                                                  *
+* Example Code:                                    *
+*   sponge(""your string here")                    *
+*                                                  *
+*                                                  *
+*                                                  *
+*    Spongebob Squarepants                         *
+*          .--..--..--..--..--..--.                *
+*        .' \  (`._   (_)     _   \                *
+*      .'    |  '._)         (_)  |                *
+*      \ _.')\      .----..---.   /                *
+*      |(_.'  |    /    .-\-.  \  |                *
+*      \     0|    |   ( O| O) | o|                *
+*       |  _  |  .--.____.'._.-.  |                *
+*       \ (_) | o         -` .-`  |                *
+*        |    \   |`-._ _ _ _ _\ /                 *
+*        \    |   |  `. |_||_|   |                 *
+*        | o  |    \_      \     |     -.   .-.    *
+*        |.-.  \     `--..-'   O |     `.`-' .'    *
+*      _.'  .' |     `-.-'      /-.__   ' .-'      *
+*    .' `-.` '.|='=.='=.='=.='=|._/_ `-'.'         *
+*    `-._  `.  |________/\_____|    `-.'           *
+*       .'   ).| '=' '='\/ '=' |                   *
+*       `._.`  '---------------'                   *
+*               //___\   //___\                    *
+*                 ||       ||                      *
+*        LGB      ||_.-.   ||_.-.                  *
+*                (_.--__) (_.--__)                 *
+***************************************************/
 
 exports.sponge = function (str) {
-    
-    if (typeof str !== 'string'){
+    // Check if the argument is a string.
+    if (typeof str !== 'string') {
+      // Alert the user if it is not a string.
       alert("What you entered was not a string please pass a string");
+      // Return null.
       return null;
     }
-
+    
+    // Allocate a new variable of type string to store the return string.
     returnStr = "";
+    // Iterate over the entire input string.
     for (let i = 0; i < str.length; i++) {
+      // Given a 50% chance, append the original or inverted case character.
       returnStr += (Math.random() >= 0.5) ? ((str[i].toLowerCase() == str[i]) ? str[i].toUpperCase() : str[i].toLowerCase()) : str[i];
     }
+    
+    // Return the new string.
     return returnStr;
 };
